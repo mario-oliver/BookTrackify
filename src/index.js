89354dc2850cddad3ea90a2f32564e2f8e890f40
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-05-18 15:52:15 -0400

    dynamically creating book objects now into our grid. Added props funcitonality for dynamic creation as well and on click handling with a parent function

--- src/index.js
+++ src/index.js
@@ -2,34 +2,48 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2,
+  },
+]
+
 const BookList = () => {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id)
+    console.log(book.author)
+  }
+
   return (
     <section className="booklist">
-      <Book />
+      {books.map((book) => (
+        <Book {...book} key={book.id} logValue={getBook} />
+      ))}
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
+  const { img, title, author, logValue, id } = props
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <button onClick={() => logValue(id)}>Find Id</button>
     </article>
   )
 }
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
-    alt="Interesting Facts For Curious Minds"
-  />
-)
-const Title = () => {
-  return <h2>Interesting Facts For Curious Minds</h2>
-}
-const Author = () => <h4>Jordan Moore </h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
