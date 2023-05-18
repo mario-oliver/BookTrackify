1f834e97e8cee8addd4d4117ed7823b3cecda9c1
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-05-18 16:10:57 -0400

    add imags to src folder for optimization

--- src/books.js
+++ src/books.js
@@ -1,3 +1,7 @@
+import AtomicHabitsImage from './images/atomic-habits-img.jpg'
+import BreakingHabitImage from './images/breakingthehabitofyourself-image.jpg'
+import lawsOfHumanNatureImage from './images/laws-of-human-nature-img.webp'
+
 export const books = [
   {
     author: 'Jordan Moore',
@@ -8,7 +12,19 @@ export const books = [
   {
     author: 'James Clear',
     title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    img: AtomicHabitsImage,
     id: 2,
+  },
+  {
+    author: 'Robert Greene',
+    title: 'Laws of Human Nature',
+    img: lawsOfHumanNatureImage,
+    id: 3,
+  },
+  {
+    author: 'Joe Dispenza',
+    title: 'Breaking The Habit of Being Yourself',
+    img: BreakingHabitImage,
+    id: 4,
   },
 ]
